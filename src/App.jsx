e2581215c3a1c725775217dae7e4158a7cb9f117
clayfriend <muminov0101@mail.ru>
2025-02-04 10:35:28 +0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import "./index.css"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
+
+
